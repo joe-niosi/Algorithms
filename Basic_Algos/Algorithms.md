a91266d753dbc92891303877032aaacbfeb3f1f6
Joseph Niosi <niosij@gmail.com>
2022-03-01 14:04:10 -0500

    first commit

---
+++ Basic_Algos/Algorithms.md
@@ -0,0 +1,3 @@
+Algorithms Practice Repository
+    -Over time I will be adding algos as a good way to practice and learn
+    -Will try to categorize them into directories based on degree of difficulty
---
+++ Basic_Algos/fundementals.js
@@ -0,0 +1,13 @@
+// Fundemental Algos to practice
+
+// #1
+// Setting and Swapping
+// Set myNumber to 42. Set myName to your name. Now swap myNumber into myName & vice versa.
+
+myName = 'Joey';
+myNumber = 42;
+console.log("myNumber = " + myNumber);
+console.log("myName = " + myName);
+
+
+
